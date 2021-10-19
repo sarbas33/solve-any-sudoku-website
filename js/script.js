8bb8323d65a318f57d56808854012be18f445ad8
sarbas33 <85018253+sarbas33@users.noreply.github.com>
2021-10-19 21:55:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -218,3 +218,100 @@ function submitted(){
  document.getElementById('c-79').value=grid[8][7];
  document.getElementById('c-80').value=grid[8][8];
 }
+
+function clearer(){
+
+  var grid=[['.','.','.','.','.','.','.','.','.'],['.','.','.','.','.','.','.','.','.'],['.','.','.','.','.','.','.','.','.'],['.','.','.','.','.','.','.','.','.'],['.','.','.','.','.','.','.','.','.'],['.','.','.','.','.','.','.','.','.'],['.','.','.','.','.','.','.','.','.'],['.','.','.','.','.','.','.','.','.'],['.','.','.','.','.','.','.','.','.']]
+  console.log(grid);
+  document.getElementById('c-0').value=grid[0][0];
+  document.getElementById('c-1').value=grid[0][1];
+  document.getElementById('c-2').value=grid[0][2];
+  document.getElementById('c-3').value=grid[0][3];
+  document.getElementById('c-4').value=grid[0][4];
+  document.getElementById('c-5').value=grid[0][5];
+  document.getElementById('c-6').value=grid[0][6];
+  document.getElementById('c-7').value=grid[0][7];
+  document.getElementById('c-8').value=grid[0][8];
+  
+  document.getElementById('c-9').value=grid[1][0];
+  document.getElementById('c-10').value=grid[1][1];
+  document.getElementById('c-11').value=grid[1][2];
+  document.getElementById('c-12').value=grid[1][3];
+  document.getElementById('c-13').value=grid[1][4];
+  document.getElementById('c-14').value=grid[1][5];
+  document.getElementById('c-15').value=grid[1][6];
+  document.getElementById('c-16').value=grid[1][7];
+  document.getElementById('c-17').value=grid[1][8];
+  
+  document.getElementById('c-18').value=grid[2][0];
+  document.getElementById('c-19').value=grid[2][1];
+  document.getElementById('c-20').value=grid[2][2];
+  document.getElementById('c-21').value=grid[2][3];
+  document.getElementById('c-22').value=grid[2][4];
+  document.getElementById('c-23').value=grid[2][5];
+  document.getElementById('c-24').value=grid[2][6];
+  document.getElementById('c-25').value=grid[2][7];
+  document.getElementById('c-26').value=grid[2][8];
+  
+  document.getElementById('c-27').value=grid[3][0];
+  document.getElementById('c-28').value=grid[3][1];
+  document.getElementById('c-29').value=grid[3][2];
+  document.getElementById('c-30').value=grid[3][3];
+  document.getElementById('c-31').value=grid[3][4];
+  document.getElementById('c-32').value=grid[3][5];
+  document.getElementById('c-33').value=grid[3][6];
+  document.getElementById('c-34').value=grid[3][7];
+  document.getElementById('c-35').value=grid[3][8];
+  
+  document.getElementById('c-36').value=grid[4][0];
+  document.getElementById('c-37').value=grid[4][1];
+  document.getElementById('c-38').value=grid[4][2];
+  document.getElementById('c-39').value=grid[4][3];
+  document.getElementById('c-40').value=grid[4][4];
+  document.getElementById('c-41').value=grid[4][5];
+  document.getElementById('c-42').value=grid[4][6];
+  document.getElementById('c-43').value=grid[4][7];
+  document.getElementById('c-44').value=grid[4][8];
+  
+  document.getElementById('c-45').value=grid[5][0];
+  document.getElementById('c-46').value=grid[5][1];
+  document.getElementById('c-47').value=grid[5][2];
+  document.getElementById('c-48').value=grid[5][3];
+  document.getElementById('c-49').value=grid[5][4];
+  document.getElementById('c-50').value=grid[5][5];
+  document.getElementById('c-51').value=grid[5][6];
+  document.getElementById('c-52').value=grid[5][7];
+  document.getElementById('c-53').value=grid[5][8];
+  
+  document.getElementById('c-54').value=grid[6][0];
+  document.getElementById('c-55').value=grid[6][1];
+  document.getElementById('c-56').value=grid[6][2];
+  document.getElementById('c-57').value=grid[6][3];
+  document.getElementById('c-58').value=grid[6][4];
+  document.getElementById('c-59').value=grid[6][5];
+  document.getElementById('c-60').value=grid[6][6];
+  document.getElementById('c-61').value=grid[6][7];
+  document.getElementById('c-62').value=grid[6][8];
+  
+  document.getElementById('c-63').value=grid[7][0];
+  document.getElementById('c-64').value=grid[7][1];
+  document.getElementById('c-65').value=grid[7][2];
+  document.getElementById('c-66').value=grid[7][3];
+  document.getElementById('c-67').value=grid[7][4];
+  document.getElementById('c-68').value=grid[7][5];
+  document.getElementById('c-69').value=grid[7][6];
+  document.getElementById('c-70').value=grid[7][7];
+  document.getElementById('c-71').value=grid[7][8];
+  
+  document.getElementById('c-72').value=grid[8][0];
+  document.getElementById('c-73').value=grid[8][1];
+  document.getElementById('c-74').value=grid[8][2];
+  document.getElementById('c-75').value=grid[8][3];
+  document.getElementById('c-76').value=grid[8][4];
+  document.getElementById('c-77').value=grid[8][5];
+  document.getElementById('c-78').value=grid[8][6];
+  document.getElementById('c-79').value=grid[8][7];
+  document.getElementById('c-80').value=grid[8][8];
+  }
+
+
